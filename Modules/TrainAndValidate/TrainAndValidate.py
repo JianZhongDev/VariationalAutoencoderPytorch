d35b52d8eac26bc1c33b6b69190f04c24aa45396
Jian Zhong <jian.zhong.dev@gmail.com>
2024-07-03 17:20:35 -0700

    update function naming

--- Modules/TrainAndValidate/TrainAndValidate.py
+++ Modules/TrainAndValidate/TrainAndValidate.py
@@ -68,7 +68,7 @@ def validate_one_epoch(
     encoder_model,
     distrib_model, 
     decoder_model,
-    train_loader,
+    validate_loader,
     data_loss_func,
     distrib_loss_rate = 0,
     distrib_loss_func = None,
@@ -82,7 +82,7 @@ def validate_one_epoch(
     decoder_model.eval()
     distrib_model.eval()
 
-    for i_batch, data in enumerate(train_loader):        
+    for i_batch, data in enumerate(validate_loader):        
         inputs, targets = data
 
         if device:
@@ -112,39 +112,3 @@ def validate_one_epoch(
     print(f"Validate: Avg loss: {avg_loss:>8f}")
 
     return avg_loss
-
-
-# # validate encoder and decoder for one epoch
-# def validate_one_epoch(
-#     encoder_model,
-#     decoder_model,
-#     validate_loader,
-#     loss_func,
-#     device = True,
-# ):
-#     tot_loss = 0.0
-#     avg_loss = 0.0
-#     tot_nof_batch = len(validate_loader)
-#     tot_samples = len(validate_loader.dataset)
-
-#     encoder_model.eval()
-#     decoder_model.eval()
-#     with torch.no_grad():
-#         for i_batch, data in enumerate(validate_loader):
-#             inputs, targets = data
-
-#             if device:
-#                 inputs = inputs.to(device)
-#                 targets = targets.to(device)
-
-#             cur_codes = encoder_model(inputs) # encode input data into codes in latent space
-#             cur_preds = decoder_model(cur_codes) # reconstruct input image
-        
-#             loss = loss_func(cur_preds, targets)
-#             tot_loss += loss.item()
-
-#     avg_loss = tot_loss/tot_nof_batch
-
-#     print(f"Validate: Avg loss: {avg_loss: > 8f}")
-
-#     return avg_loss
